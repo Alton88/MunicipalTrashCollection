5b74adb63570a57df8067a206097e1d374dafb3a
Alton88 <altonyee@gmail.com>
2016-11-08 14:11:30 -0600

    Added google map

--- MunicipalTrashCollection/Controllers/CustomerController.cs
+++ MunicipalTrashCollection/Controllers/CustomerController.cs
@@ -32,15 +32,30 @@ namespace MunicipalTrashCollection.Controllers
 
             return View();
         }
-        public ActionResult EditPickUpInfo(int? id)
+        [HttpGet]
+        public ActionResult PausePickUpService(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customer = db.Addresses.Include(a => a.Customer).Include(d => d.Day).Single(c => c.Customer.Id == id);
 
-            return View();
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
         }
-        public ActionResult EditPickUpInfo(Customer customer)
+        [HttpPost]
+        public ActionResult PausePickUpService(Day day)
         {
-
-            return View();
+            var userEmail = User.Identity.Name;
+            var customer = db.Addresses.Include(a => a.Customer).Include(d => d.Day).Single(c => c.Customer.EmailAddress == userEmail);
+            customer.Day.StartVacation = day.StartVacation.Value.Date;
+            customer.Day.EndVacation = day.EndVacation.Value.Date;
+            db.SaveChanges();
+            return Content($"Vacation start date {customer.Day.StartVacation.Value.ToShortDateString()} and vacation end date {customer.Day.EndVacation.Value.ToShortDateString()}");
         }
         [HttpGet]
         public ActionResult TempPickUpDay(int? id)
@@ -60,8 +75,12 @@ namespace MunicipalTrashCollection.Controllers
         [HttpPost]
         public ActionResult TempPickUpDay(Day pickUpDate)
         {
-            
-            return Content(pickUpDate.PickUpDateChange.ToString());
+            //DateTime day = pickUpDate.PickUpDateChange.Value.Date;
+            var userEmail = User.Identity.Name;
+            var customer = db.Addresses.Include(a => a.Customer).Include(d => d.Day).Single(c => c.Customer.EmailAddress == userEmail);
+            customer.Day.PickUpDateChange = pickUpDate.PickUpDateChange.Value.Date;
+            db.SaveChanges();
+            return Content(customer.Day.PickUpDateChange.Value.ToShortDateString());
         }
         [HttpGet]
         public ActionResult EditCustomerDay(int? id)
